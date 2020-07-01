0ee4dd6f6211e575ea21c7e1ddeb4a8d3eb7af6f
Yiheng <development.henryquan@gmail.com>
2020-07-01 21:38:18 +1000

    read from pattern.txt

--- solver.cpp
+++ solver.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 /**
  * seal is a tape over a row and orbs cannot move,
@@ -6,7 +8,17 @@
  */
 enum orbs { empty, fire, water, wood, light, dark, recovery, obstacle, bomb, poison, poison_plus, seal, disabled };
 
+// Maybe a good idea to consider the weight
+
 int main() {
-    std::cout << "Hello World\n";
+    using namespace std;
+    ifstream patternFile("pattern.txt");
+    string lines;
+    while (getline(patternFile, lines)) {
+        // Ignore lines that start with `//`
+        if (lines.find("//") == 0) continue;
+        cout << lines << endl;
+    }
+    patternFile.close();
     return 0;
 }
