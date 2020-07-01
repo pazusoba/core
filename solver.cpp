e84aba7a4eddfced1645fb431624dff2e2ff4c34
Yiheng <development.henryquan@gmail.com>
2020-07-01 21:26:30 +1000

    added patterns and orbs

--- solver.cpp
+++ solver.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/**
+ * seal is a tape over a row and orbs cannot move,
+ * disabled means it will not get erased even if connected.
+ */
+enum orbs { empty, fire, water, wood, light, dark, recovery, obstacle, bomb, poison, poison_plus, seal, disabled };
+
 int main() {
     std::cout << "Hello World\n";
     return 0;
