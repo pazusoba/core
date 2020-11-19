00da1b6fc74f7ce273eebbdb3ae7a7074754892c
Yiheng <development.henryquan@gmail.com>
2020-11-19 22:38:18 +1100

    三蔵法師
    15000 size, but less than 2s. It is crazy good. I will probably try multiplay at a time.
    
    
    Former-commit-id: 29f710e6fd651b86f4d09500e21c4699d00d5c13

--- core/v1/solver.cpp
+++ core/v1/solver.cpp
@@ -78,9 +78,9 @@ std::vector<Route> PSolver::solve()
 //        new PlusProfile({pad::light, pad::dark}),
 //        new ColourProfile({pad::light, pad::dark})};
     // Amen
-    std::vector<Profile *> profiles{
-        new ComboProfile(7),
-        new OrbProfile(1)};
+    // std::vector<Profile *> profiles{
+    //     new ComboProfile(7),
+    //     new OrbProfile(1)};
     // Amen & +
     //  std::vector<Profile *>
     //      profiles{
@@ -89,7 +89,7 @@ std::vector<Route> PSolver::solve()
     //          new PlusProfile({pad::fire, pad::water, pad::wood, pad::light, pad::dark}),
     //          new OneColumnProfile};
     // Combo only
-    // std::vector<Profile *> profiles{new ComboProfile};
+    std::vector<Profile *> profiles{new ComboProfile};
     // Laou
     // std::vector<Profile *> profiles{
     //     new ComboProfile,
