bcff71a9d54afca94822012a3beb9567f6a006c0
Yiheng <development.henryquan@gmail.com>
2021-05-27 21:11:36 +1000

    tried to get board location automatically
    Detection is not simple I guess

--- automation/board_location.py
+++ automation/board_location.py
@@ -1,11 +1,48 @@
-"""
-Get the board location
-"""
 import pyautogui as gui
 
-input('Move to top left of the board and press enter')
-one = gui.position()
-input('Move to bottom right of the board and press enter')
-two = gui.position()
+import cv2 as cv
+import numpy as np
 
-print("[{}, {}, {}, {}]".format(one.x, one.y, two.x, two.y))
+import mss
+import mss.tools
+
+def get_location_manually():
+    """
+    Get the board location on the screen
+    """
+    input('Move to top left of the board and press enter')
+    one = gui.position()
+    input('Move to bottom right of the board and press enter')
+    two = gui.position()
+
+    print("Board Location - [{}, {}, {}, {}]".format(one.x, one.y, two.x, two.y))
+    return [one.x, one.y, two.x, two.y]
+
+def clearAllWindows():
+    cv.waitKey()
+    cv.destroyAllWindows()
+
+def get_location_automatically():
+    """
+    Get the board location automatically on the screen
+    """
+    border = 16
+    with mss.mss() as screen:
+        raw = screen.grab(screen.monitors[0])
+        raw_cv = np.array(raw)
+        # add some borders around
+        raw_cv = cv.copyMakeBorder(raw_cv, border, border, border, border, cv.BORDER_CONSTANT)
+        gray = cv.cvtColor(raw_cv, cv.COLOR_BGR2GRAY)
+        _, binary = cv.threshold(gray, 40, 255, cv.THRESH_BINARY)
+
+        contours, _ = cv.findContours(binary, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+        rectContours = [c for c in contours if  cv.contourArea(c) > 3000000]
+        result = cv.drawContours(raw_cv, rectContours, -1, (255, 0, 255), 3)
+        for contour in rectContours:
+            print(contour)
+
+        cv.imshow("fullscreen", result)
+        clearAllWindows()
+
+get_location_manually()
+# get_location_automatically()
