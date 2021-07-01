e7c0afbaf5e7395e82bff33310a8e011ff0e4ff9
Yiheng <development.henryquan@gmail.com>
2021-07-01 19:34:41 +1000

    removed automation
    moved to pazuauto

--- automation/game/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-from glob import iglob
-
-# replace .PNG to .png on Linux
-root_dir = "**"
-files = [f for f in iglob(root_dir, recursive=True) if f.endswith(".PNG")]
-for f in files:
-    os.rename(f, f.replace(".PNG", ".png"))
