8d0c70ef8eff36b895b7f7bf0812b40fe41239db
Yiheng <development.henryquan@gmail.com>
2021-07-03 22:12:40 +1000

    updated formats

--- archived/core/v1/solver.h
+++ archived/core/v1/solver.h
@@ -16,38 +16,38 @@
 extern "C"
 {
 
-class PSolver
-{
-    int row = 0;
-    int column = 0;
-    int minErase = 3;
-    int steps = 25;
-    int size = 1000;
-    bool debug = false;
+    class PSolver
+    {
+        int row = 0;
+        int column = 0;
+        int minErase = 3;
+        int steps = 25;
+        int size = 1000;
+        bool debug = false;
 
-    /// Read board from filePath, return the board
-    Board readBoard(const std::string &filePath);
+        /// Read board from filePath, return the board
+        Board readBoard(const std::string &filePath);
 
-public:
-    /// This is the original board
-    PBoard board;
+    public:
+        /// This is the original board
+        PBoard board;
 
-    PSolver(const std::string &filePath, int minErase, int steps, int size);
+        PSolver(const std::string &filePath, int minErase, int steps, int size);
 
-    /// Solve the current board
-    std::vector<Route> solve();
+        /// Solve the current board
+        std::vector<Route> solve();
 
-    /// Read a board from a string
-    void setBoardFrom(const std::string &board);
+        /// Read a board from a string
+        void setBoardFrom(const std::string &board);
 
-    /// A random board that is row x column
-    void setRandomBoard(int row, int column);
+        /// A random board that is row x column
+        void setRandomBoard(int row, int column);
 
-    /// Update beam size, mainly for Qt
-    void setBeamSize(int size);
+        /// Update beam size, mainly for Qt
+        void setBeamSize(int size);
 
-    /// Update step limit
-    void setStepLimit(int step);
-};
+        /// Update step limit
+        void setStepLimit(int step);
+    };
 }
 #endif
--- archived/core/v1/state.h
+++ archived/core/v1/state.h
@@ -12,56 +12,56 @@
 extern "C"
 {
 
-class PState
-{
-    // A list of state
-    typedef std::vector<PState *> PStateList;
+    class PState
+    {
+        // A list of state
+        typedef std::vector<PState *> PStateList;
 
-    // This is max step we want to go (termination point)
-    int maxStep;
-    // ALl possible moves from this state
-    PStateList children;
-    std::mutex mtx;
+        // This is max step we want to go (termination point)
+        int maxStep;
+        // ALl possible moves from this state
+        PStateList children;
+        std::mutex mtx;
 
-    // This is a recursive function to print from the beginning
-    void printStateFromRoot(const PState *parent);
-    // This is a recursive function to save state from the beginning
-    void saveStateFromRoot(const PState *parent, std::ofstream* file);
+        // This is a recursive function to print from the beginning
+        void printStateFromRoot(const PState *parent);
+        // This is a recursive function to save state from the beginning
+        void saveStateFromRoot(const PState *parent, std::ofstream *file);
 
-public:
-    // The board
-    PBoard board;
-    PBoard erasedBoard;
-    // This score shows how good the current board is
-    int score = 0;
-    // This is the number of steps (depth)
-    int step;
-    // This is the previous orb and it moved to current orb
-    // Current must not move back to parent location
-    OrbLocation previous;
-    // This is current orb which is active (the player is holding it)
-    OrbLocation current;
-    // Save the parent's address to track back
-    PState *parent = nullptr;
+    public:
+        // The board
+        PBoard board;
+        PBoard erasedBoard;
+        // This score shows how good the current board is
+        int score = 0;
+        // This is the number of steps (depth)
+        int step;
+        // This is the previous orb and it moved to current orb
+        // Current must not move back to parent location
+        OrbLocation previous;
+        // This is current orb which is active (the player is holding it)
+        OrbLocation current;
+        // Save the parent's address to track back
+        PState *parent = nullptr;
 
-    // This is only for testing
-    PState(int s) : score(s) {}
-    PState(const PBoard &board, const OrbLocation &from, const OrbLocation &to, int step, int maxStep);
-    ~PState();
+        // This is only for testing
+        PState(int s) : score(s) {}
+        PState(const PBoard &board, const OrbLocation &from, const OrbLocation &to, int step, int maxStep);
+        ~PState();
 
-    // Override operators
-    bool operator<(const PState &a) const;
-    bool operator>(const PState &a) const;
-    bool operator==(const PState &a) const;
+        // Override operators
+        bool operator<(const PState &a) const;
+        bool operator>(const PState &a) const;
+        bool operator==(const PState &a) const;
 
-    // This returns a list of all possible children states
-    PStateList getChildren();
+        // This returns a list of all possible children states
+        PStateList getChildren();
 
-    // Print out current state, its board, score and more
-    void printState();
+        // Print out current state, its board, score and more
+        void printState();
 
-    // Save the entire state from begining to the end to the disk
-    void saveToDisk();
-};
+        // Save the entire state from begining to the end to the disk
+        void saveToDisk();
+    };
 }
 #endif
