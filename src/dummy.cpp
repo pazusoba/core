9b475142dbcf3d180175dc091d8bca21982efd6d
Yiheng <development.henryquan@gmail.com>
2021-07-04 15:34:52 +1000

    added fmt module

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "pazusoba.h"
+#include <fmt/core.h>
 
 int main()
 {
+    fmt::print("The answer is {}.\n", 42);
     int a = 0;
     const unsigned char *hello = (const unsigned char *)"Hello World";
     printf("Hello, CMake. Hash is %u\n", pazusoba::hash::djb2_hash(hello));
