518c86184e10087cd91812cc321904d9955869b0
Yiheng <development.henryquan@gmail.com>
2021-07-04 14:46:40 +1000

    added compiler flags for gcc and msvc

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    int a = 0;
     const unsigned char *hello = (const unsigned char *)"Hello World";
     printf("Hello, CMake. Hash is %u\n", pazusoba::hash::djb2_hash(hello));
     return 0;
