376f7a2a7748d8b74d7f669f3b0382f4fd3d4886
Yiheng <development.henryquan@gmail.com>
2021-07-04 15:45:39 +1000

    removed stdio.h and use <fmt/core.h> from now on
    moved headers to pazusoba folder so it becomes #include <pazusoba/core.h>

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include "pazusoba.h"
+#include <pazusoba/core.h>
 #include <fmt/core.h>
 
 int main()
@@ -7,6 +6,6 @@ int main()
     fmt::print("The answer is {}.\n", 42);
     int a = 0;
     const unsigned char *hello = (const unsigned char *)"Hello World";
-    printf("Hello, CMake. Hash is %u\n", pazusoba::hash::djb2_hash(hello));
+    fmt::print("Hello, CMake. Hash is {}.\n", pazusoba::hash::djb2_hash(hello));
     return 0;
 }
