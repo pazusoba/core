69936b3ea05eb4d588c7b6886106d56145795a4b
Yiheng <development.henryquan@gmail.com>
2021-08-29 20:54:24 +1000

    working on multithreading

--- src/solver/beam_search.cpp
+++ src/solver/beam_search.cpp
@@ -2,6 +2,8 @@
 #include <pazusoba/queue.h>
 #include <pazusoba/solver/beam_search.h>
 #include <pazusoba/state.h>
+#include <mutex>
+#include <thread>
 #include <unordered_map>
 
 namespace pazusoba {
@@ -9,8 +11,13 @@ Route BeamSearch::solve() {
     const Board& board = _parser.board();
     StatePriorityQueue pq;
     std::unordered_map<size_t, bool> visited;
-
-    auto push = [&](const State& state) { pq.push(state); };
+    std::mutex mtx;
+    std::deque<State> toVisit;
+    auto push = [&](const State& state) {
+        mtx.lock();
+        toVisit.push_back(state);
+        mtx.unlock();
+    };
 
     // setup all the initial states
     auto maxSteps = _parser.maxSteps();
@@ -19,31 +26,59 @@ Route BeamSearch::solve() {
         pq.emplace(board, maxSteps, i);
     }
 
+    // setup multithreading
+    std::deque<std::thread> threads;
+    // Save one core for now
+    pint processor_count = std::thread::hardware_concurrency();
+    // processor_count = 0;
+    if (processor_count == 0)
+        processor_count = 1;
+    fmt::print("Using {} threads\n", processor_count);
+
     // Use Beam Search starting from step one
     State bestState = pq.top();
-    auto beamSize = _parser.beamSize();
+    auto beamSize = _parser.beamSize() / processor_count + 1;
     for (pint i = 0; i < maxSteps; i++) {
-        for (pint j = 0; j < beamSize; j++) {
-            if (pq.empty())
-                break;
+        fmt::print("step {}\n", i + 1);
+        for (pint j = 0; j < processor_count; j++) {
+            threads.emplace_back([&] {
+                for (pint k = 0; k < beamSize; k++) {
+                    if (pq.empty())
+                        return;
 
-            auto current = pq.top();
-            pq.pop();
+                    mtx.lock();
+                    auto current = pq.top();
+                    pq.pop();
+                    mtx.unlock();
 
-            auto hash = current.hash();
-            if (visited[hash]) {
-                continue;
-            } else {
-                visited[hash] = true;
-            }
+                    auto hash = current.hash();
+                    if (visited[hash]) {
+                        // check more since this one is already checked
+                        // j -= 1;
+                        continue;
+                    } else {
+                        visited[hash] = true;
+                    }
 
-            auto score = current.score();
-            if (score > bestState.score()) {
-                bestState = current;
-            }
-
-            current.children(push, false);
+                    current.children(push, false);
+                }
+            });
         }
+
+        for (auto& t : threads)
+            t.join();
+        threads.clear();
+
+        for (const auto& state : toVisit) {
+            pq.push(state);
+            auto score = state.score();
+            if (score > 0)
+                fmt::print("score {}", score);
+            if (score > bestState.score()) {
+                bestState = state;
+            }
+        }
+        toVisit.clear();
     }
 
     fmt::print("best score: {}\n", bestState.score());
