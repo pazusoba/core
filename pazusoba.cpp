7652a4d7a9822049476fa66b00b31644075fbfa9
Yiheng <development.henryquan@gmail.com>
2021-12-29 15:45:39 +1100

    all in one cpp file

---
+++ pazusoba.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file pazusoba.cpp
+ * @author Yiheng Quan
+ * @brief All in one file to prevent unnecessary copying. This will ideally
+ * improve the performance of the program.
+ * @version 0.1
+ * @date 2021-12-29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
