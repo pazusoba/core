2ee7639896e99c10ee3f0ae919aae7d199d0abbd
Yiheng <development.henryquan@gmail.com>
2022-02-28 19:44:24 +1100

    added timer to track the time

--- experiment/main.cpp
+++ experiment/main.cpp
@@ -1,8 +1,10 @@
 #include "pazusoba.h"
+#include "timer.h"
 
 int main(int argc, char* argv[]) {
     auto solver = pazusoba::solver();
     solver.parse_args(argc, argv);
+    pazusoba::Timer timer("explore");
     solver.explore();
     return 0;
 }
