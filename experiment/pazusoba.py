a87beaeba70738118cc1fd328b86348c7ce70c3a
Yiheng <development.henryquan@gmail.com>
2022-02-28 23:57:56 +1100

    wrap raw c object in python

--- experiment/pazusoba.py
+++ experiment/pazusoba.py
@@ -20,13 +20,64 @@ class c_state(Structure):
                 ("routes", c_location*151)]
 
 
+class Location:
+    def __init__(self, raw):
+        self.row = raw.row
+        self.column = raw.column
+
+    def __str__(self):
+        return "({}, {})".format(self.row, self.column)
+
+    def __repr__(self):
+        return "({}, {})".format(self.row, self.column)
+
+
+class State:
+    def __init__(self, raw):
+        self.combo = raw.combo
+        self.max_combo = raw.max_combo
+        self.step = raw.step
+        self.row = raw.row
+        self.column = raw.column
+        self.routes = [Location(raw.routes[i])
+                       for i in range(151) if raw.routes[i].row != -1]
+
+        # shorten routes
+        # insert the first
+        self.simplified_routes = [self.routes[0]]
+        length = len(self.routes)
+
+        # loop from the second step
+        for i in range(1, length - 1):
+            x0 = self.routes[i].row - self.routes[i - 1].row
+            x1 = self.routes[i + 1].row - self.routes[i].row
+            if x0 == x1:
+                y0 = self.routes[i].column - self.routes[i - 1].column
+                y1 = self.routes[i + 1].column - self.routes[i].column
+                if y0 == y1:
+                    continue
+            self.simplified_routes.append(self.routes[i])
+
+        # insert the last
+        self.simplified_routes.append(self.routes[-1])
+        self.simplified_step = len(self.simplified_routes)
+
+    def __str__(self):
+        return "Combo: {}\nMax Combo: {}\nStep: {} ({})\nRow: {}\nColumn: {}\nRoutes: {}\nSimplified Routes: {}".format(
+            self.combo, self.max_combo, self.step, self.simplified_step, self.row, self.column, self.routes, self.simplified_routes)
+
+    def __repr__(self):
+        return "Combo: {}\nMax Combo: {}\nStep: {} ({})\nRow: {}\nColumn: {}\nRoutes: {}\nSimplified Routes: {}".format(
+            self.combo, self.max_combo, self.step, self.simplified_step, self.row, self.column, self.routes, self.simplified_routes)
+
+
 libpazusoba = CDLL("libpazusoba.so", winmode=0)
 # NOTE: the restype here must be correct to call it properly
 libpazusoba.explore.restype = c_state
 libpazusoba.explore.argtypes = (c_int, POINTER(c_char_p))
 
 
-def explore(arguments: List[str]) -> c_state:
+def explore(arguments: List[str]) -> State:
     # additional step is required here because Mac is stricter than Windows
     argv = []
     for s in arguments:
@@ -36,14 +87,10 @@ def explore(arguments: List[str]) -> c_state:
     c_argv[:] = argv
 
     state = libpazusoba.explore(c_argc, c_argv)
-    return state
+    return State(state)
 
 
 if __name__ == "__main__":
-    s = explore(
-        ["pazusoba", "RLRRDBHBLDBLDHRGLGBRGLBDBHDGRL", "3", "100", "500"])
-    print(s.combo, s.max_combo, s.step, s.row, s.column)
-    for i in range(150):
-        if i > s.step:
-            break
-        print(s.routes[i].row, s.routes[i].column)
+    state = explore(
+        ["pazusoba", "RLRRDBHBLDBLDHRGLGBRGLBDBHDGRL", "3", "100", "5000"])
+    print(state)
