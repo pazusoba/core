fbf2da61a4fb43cf5652cb09fe0bdd66dc4a8ab0
Yiheng <development.henryquan@gmail.com>
2022-03-07 22:22:48 +1100

    setup.py doesn't work on windows

--- experiment/setup.py
+++ experiment/setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup, Extension
+import platform
+
+compilor_args = ['-std=c++11', '-O2']
+if platform.system() == 'Windows':
+    compilor_args = []
 
 module_pazusoba = Extension('pazusoba',
-                    sources = ['pazusoba.cpp', 'api.cpp'],
-                    extra_compile_args=['-std=c++11', '-O2'])
+                            sources=['pazusoba.cpp', 'api.cpp'],
+                            extra_compile_args=compilor_args)
 
-setup(name = 'pazusoba',
-       version = '1.0.0',
-       license='MIT',
-       url='https://github.com/pazusoba',
-       description = 'Solve Puzzle & Dragons with C++ in Python',
-       ext_modules = [module_pazusoba])
+setup(name='pazusoba',
+      version='1.0.0',
+      license='MIT',
+      url='https://github.com/pazusoba',
+      description='Solve Puzzle & Dragons with C++ in Python',
+      ext_modules=[module_pazusoba])
